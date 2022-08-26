a11cf355b61ad2c47f9c1a12df2097bd3d955200
Edward Guilfoyle <Edward Guilfoyle>
2022-08-26 19:07:49 +1000

    Added basic sprite from itch.io, added simple player script with movement.

---
+++ client/Scenes/MainScenes/Player.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+
+var SPEED = 300
+var velocity = Vector2(0, 0)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta) -> void:
+	var x_input = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	var y_input = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
+	
+	velocity = Vector2(x_input, y_input).normalized()
+	move_and_slide(velocity * SPEED)
