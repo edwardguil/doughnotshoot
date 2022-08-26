704d35364e2eae83ddafbac9b1641c94236667d0
Edward Guilfoyle <edwardguil96@outlook.com>
2022-08-26 20:23:30 +1000

    Added ability for player to change weapon

--- client/Scenes/Support Scenes/Weapon.gd
+++ client/Scenes/Support Scenes/Weapon.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
-var weapons = ["Gun1"]
+var weapons = ["Gun1", "Gun2"]
 var current_weapon = 0
 
 func FireWeapon(mouse_position):
 	get_node(weapons[current_weapon]).fire(mouse_position)
 
+func ChangeWeapon():
+	get_node(weapons[current_weapon]).hide()
+	current_weapon = (current_weapon + 1) % len(weapons)
+	get_node(weapons[current_weapon]).show()
+	
 func FlipSprite(val):
 	if val:
 		$Gun1/Sprite.flip_v = true
