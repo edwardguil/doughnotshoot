b3922ef47050586aaf1488968f0ddb14838d687e
Edward Guilfoyle <Edward Guilfoyle>
2022-08-26 21:48:09 +1000

    Added commands to make bullet fire, exit position not updating correctly

--- client/Scenes/Support Scenes/Gun1.gd
+++ client/Scenes/Support Scenes/Gun1.gd
@@ -1,16 +1,14 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var BULLET_PATH = preload("res://Scenes/Support Scenes/Bullet1.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func fire(mouse_position):
+	var bullet1 = BULLET_PATH.instance()
+	bullet1.position = $Sprite1/Exit1.global_position
+	bullet1.rotation = get_parent().rotation
+	print(bullet1.position)
+	get_parent().get_parent().add_child(bullet1)
--- client/Scenes/Support Scenes/Weapon.gd
+++ client/Scenes/Support Scenes/Weapon.gd
@@ -5,16 +5,18 @@ var current_weapon = 0
 
 func FireWeapon(mouse_position):
 	get_node(weapons[current_weapon]).fire(mouse_position)
+	print($Gun1/Sprite1/Exit1.global_position)
+	print($Gun2/Sprite2/Exit2.global_position)
 
 func ChangeWeapon():
 	get_node(weapons[current_weapon]).hide()
 	current_weapon = (current_weapon + 1) % len(weapons)
 	get_node(weapons[current_weapon]).show()
-	
+
 func FlipSprite(val):
 	if val:
-		$Gun1/Sprite.flip_v = true
-		$Gun2/Sprite.flip_v = true
+		$Gun1/Sprite1.flip_v = true
+		$Gun2/Sprite2.flip_v = true
 	else:
-		$Gun1/Sprite.flip_v = false
-		$Gun2/Sprite.flip_v = false
+		$Gun1/Sprite1.flip_v = false
+		$Gun2/Sprite2.flip_v = false
